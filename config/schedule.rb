3d1481319e7ef98b22d2f2b046b68e5a6c898668
高山幸大 <takayamamiyukidai@takayamamiyukidainoMacBook-Pro-2.local>
2021-02-08 22:43:18 +0900

    本番環境でcronを実行できるように設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 
+ENV.each { |k, v| env(k, v) }
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
+env :PATH, ENV['PATH']
 
 if rails_env.to_sym == :production
-  every 10.minutes do
+  every 15.minutes do
     rake 'moneyforward:import'
   end
   every 1.day, at: '10:00 am' do
