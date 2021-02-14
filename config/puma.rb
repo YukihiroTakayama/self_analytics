a4cb4bbc8f4159a8e76c0741786da176b1d12f96
高山幸大 <takayamamiyukidai@takayamamiyukidainoMacBook-Pro-2.local>
2021-02-15 08:45:21 +0900

    fix: deploy settings

--- config/puma.rb
+++ config/puma.rb
@@ -13,3 +13,6 @@ environment ENV.fetch('RACK_ENV') { 'development' }
 on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
+
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
